907ae6b7c63efc14c8d11708011e32e861cca979
aysenurdemird <aysenurdemir.de@gmail.com>
2022-10-05 20:01:39 +0300

    week 2

---
+++ inheritanceDemo/src/inheritanceDemo/KrediUI.java
@@ -0,0 +1,7 @@
+package inheritanceDemo;  //UI = User Interface / arayüz
+
+public class KrediUI {
+	public void KrediHesapla(BaseKrediManager baseKrediManager) {
+		baseKrediManager.Hesapla();
+	}
+}
